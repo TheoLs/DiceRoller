fc0e31913fead48673ea99551812b21d79278c46
TheoLs <1739399@brunel.ac.uk>
2019-10-17 15:50:57 +0100

    Innit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.diceroller;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void on_button_click(View view){
+        TextView tv = this.findViewById(R.id.numberTextView); //alt + enter imports missing files
+
+        Random r = new Random();
+        int number = r.nextInt(7 - 1) +1;
+
+        //
+        TextView guess = this.findViewById(R.id.guess);
+        String guessedNum = guess.getText().toString();
+
+        TextView result = this.findViewById(R.id.result);
+        String yourresult = result.getText().toString();
+
+        int count = Integer.parseInt(yourresult);
+
+
+        try {
+            tv.setText(number);
+
+        } catch (Exception ex) {
+            tv.setText(Integer.toString(number));
+            if(Integer.parseInt(guessedNum) == number){
+                count++;
+            }
+        }
+
+    }
+}
